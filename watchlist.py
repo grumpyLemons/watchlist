5e49d6fe876176a77739b97925e87e2359efbd40
grumpyLemons <mark.gulev@gmail.com>
2020-07-06 13:14:44 +0700

    Added --list argument

--- watchlist.py
+++ watchlist.py
@@ -3,20 +3,32 @@
 import argparse
 import json
 import random
+from string import capwords
 
 
-def add(movie):
-    watchlist.append(movie)
+def add(movies):
+    for movie in ' '.join(movies).split('; '):
+        watchlist.append(capwords(movie))
     d_file = open('movies.json', 'w')
-    json.dump(watchlist, file)
+    json.dump(watchlist, d_file)
     d_file.close()
 
 
-def remove(movie):
-    watchlist.pop(watchlist.find(movie))
+def remove(movies):
+    for movie in ' '.join(movies).split('; '):
+        try:
+            watchlist.pop(watchlist.index(capwords(movie)))
+        except (ValueError):
+            print(f"There is no movie called {movie}")
+
     d_file = open('movies.json', 'w')
-    json.dump(watchlist, file)
+    json.dump(watchlist, d_file)
     d_file.close()
+
+
+def listm():
+    for movie in watchlist:
+        print(movie)
 
 
 file = open('movies.json', 'r')
@@ -25,16 +37,21 @@ watchlist = list(set(watchlist))
 file.close()
 
 parser = argparse.ArgumentParser(description='Watchlist')
-parser.add_argument("-a", "--add", help="Add movie to the watchlist")
-parser.add_argument("-r", "--rm", help="Removes movie from the watchlist")
+parser.add_argument(
+    "-a", "--add", help="Add movie to the watchlist", nargs='*')
+parser.add_argument(
+    "-r", "--rm", help="Removes movie from the watchlist", nargs='*')
+parser.add_argument(
+    "-l", "--list", help="Lists all movies from the list", action='store_true')
 args = parser.parse_args()
-
 try:
     if args.add:
         add(args.add)
     elif args.rm:
         remove(args.rm)
+    elif args.list:
+        listm()
     else:
         print(random.choice(watchlist))
-except PermissionError:
-    print('permission err')
+except (PermissionError, IndexError):
+    pass
